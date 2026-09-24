b213d6cf2dc8130aa04e63a69560872f27b9406a
tjistheone <tcullinan@ufl.edu>
2026-09-24 15:39:53 -0400

    Fix functions

---
+++ bitops.cpp
@@ -0,0 +1,25 @@
+
+bool getBit(unsigned int x, int n) { return ((x >> n) & 1); }
+
+unsigned int setBit(unsigned int x, int n) { return ((1u << n) | x); }
+
+unsigned int clearBit(unsigned int x, int n) { return (~(1u << n) & x); }
+
+unsigned int toggleBit(unsigned int x, int n) { return ((1u << n) ^ x); }
+
+int countOnes(unsigned int x) {
+  int total = 0;
+  for (int i = 0; i <= 31; ++i) {
+    if ((x >> i) & 1) {
+      ++total;
+    }
+  }
+  return total;
+}
+
+bool isPowerTwo(unsigned int x) {
+  if (countOnes(x) == 1) {
+    return true;
+  }
+  return false;
+}
